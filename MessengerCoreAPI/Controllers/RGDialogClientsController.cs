4725660b99ec20c580570132679c26f80e11faf7
Inar <Inar2000s@mail.ru>
2022-09-15 16:48:22 +0300

    init

---
+++ MessengerCoreAPI/Controllers/RGDialogClientsController.cs
@@ -0,0 +1,23 @@
+﻿using MessengerCoreAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MessengerCoreAPI.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class RGDialogClientsController : ControllerBase
+{
+    [HttpPost]
+    public ActionResult<Guid> FindDialog(FindDialogDto findDialogDto)
+    {
+        var dialogsClients = new RGDialogsClients().Init();
+
+        var dialog = dialogsClients
+            .GroupBy(dc => dc.IDRGDialog)
+            .FirstOrDefault(dialog => findDialogDto.ClientsIds
+                .All(c => dialog.Any(d => d.IDClient == c)))
+            ?.First().IDRGDialog;
+
+        return dialog ?? Guid.Empty;
+    }
+}
---
+++ MessengerCoreAPI/Models/FindDialogDto.cs
@@ -0,0 +1,6 @@
+﻿namespace MessengerCoreAPI.Models;
+
+public class FindDialogDto
+{
+    public List<Guid> ClientsIds { get; set; } = new();
+}
